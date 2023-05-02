aac2941514e5324b9f846b18944f87fc973a0013
JonasL <460363851@qq.com>
2023-05-02 21:36:18 +0800

    Update lean-2.sh

--- scripts/lean-2.sh
+++ scripts/lean-2.sh
@@ -132,7 +132,7 @@ popd
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 # sed -i 's/5.15/5.10/g' target/linux/rockchip/Makefile
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # 风扇脚本
 sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
